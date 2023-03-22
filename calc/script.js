6a5379e1550db88108363c259044d00e13c3d3fc
cfan310 <cfanning43@gmail.com>
2023-03-21 17:06:38 -0700

    added exterior code from calculator file

---
+++ calc/script.js
@@ -0,0 +1,47 @@
+// operand (number) btns by id
+let sevenBtn = document.getElementById('btn-7')
+let eightBtn = document.getElementById('btn-8')
+let nineBtn = document.getElementById('btn-9')
+let fourBtn = document.getElementById('btn-4')
+let fiveBtn = document.getElementById('btn-5')
+let sixBtn = document.getElementById('btn-6')
+let oneBtn = document.getElementById('btn-1')
+let twoBtn = document.getElementById('btn-2')
+let threeBtn = document.getElementById('btn-3')
+let zeroBtn = document.getElementById('btn-0')
+
+// operator btns by id
+let timesBtn = document.getElementById('btn-times');
+let divideBtn = document.getElementById('btn-divide');
+let plusBtn = document.getElementById('btn-plus');
+let minusBtn = document.getElementById('btn-minus');
+let equalBtn = document.getElementById('btn-equal');
+
+// itneger values assigned to operand buttons 
+sevenValue = 7;
+eightValue = 8;
+nineValue = 9;
+fouValue = 4;
+fiveValue = 5;
+sixValue = 6;
+oneValue = 1; 
+twoValue = 2;
+threeValue = 3;
+zeroValue = 0;
+
+
+// first, let's click 9 and display it somewhere on the page 
+
+nineBtn.addEventListener('click', function displayNine(){
+    output.innerHTML = nineBtn;
+}); 
+
+// create a screen variable, where we will display the text of the butto clicked
+// using its variable, not its innerHTML or innerText since we need an integer and not a string 
+
+let output = document.getElementById('output'); 
+ 
+output.innerHTML = 5 + 8; 
+
+// give all buttons the same class
+
