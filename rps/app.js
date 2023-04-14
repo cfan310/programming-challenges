f9c28bfd5f2fd9b119fab8f0b5a9619c0aeb958e
cfan310 <cfanning43@gmail.com>
2023-04-13 17:17:30 -0700

    added single-game browser functionality. next need to run 5 games, with counters, and add innerHTML without replacing the old innerHTML

--- rps/app.js
+++ rps/app.js
@@ -1,3 +1,4 @@
+/*
 // returns either rock, paper, or scissors 
 function getComputerChoice() {  
     // assign random num 0-2 0 is rock 1 is paper 2 is scissors
@@ -104,4 +105,113 @@ function playFiveRounds() {
    
 }
 
-playFiveRounds();
+//playFiveRounds();
+*/
+
+// BROWSER GAME LOGIC   
+
+// to begin the game, (a single game), the player clicks either the rock, paper, or scissors button 
+// 
+
+// rock, paper, scissors buttons stored as variables 
+let rockBtn = document.getElementById('rockBtn');
+let paperBtn = document.getElementById('paperBtn');
+let scissorsBtn = document.getElementById('scissorsBtn');
+
+// if player clicks rock, player will be assigned 'rock' as his selection
+function getPlayerChoice() {
+    let playerChoice = prompt('Please choose rock, paper, or scissors').toLowerCase();
+    console.log(`Player chose ${playerChoice}`);
+    return playerChoice; 
+}
+
+// returns either rock, paper, or scissors 
+function getComputerChoice() {  
+    // assign random num 0-2 0 is rock 1 is paper 2 is scissors
+    // return the choice
+    let num = Math.floor(Math.random() * 3);
+    let computerChoice;
+    // return num; returns either 0, 1, or 2
+    // if 0, computerChoice = 1 = rock
+    if (num == 0) {
+        computerChoice = 'rock'; 
+    } else if (num == 1) {
+        computerChoice = 'paper';
+    } else if (num == 2) {
+        computerChoice = 'scissors'; 
+    }
+    console.log(`Computer chose ${computerChoice}`);
+    return computerChoice;   
+}
+
+let playerChoice = 'rock'
+let computerChoice = 'scissors';
+
+let playerChoiceLog = document.getElementById('playerChoiceLog'); 
+let computerChoiceLog = document.getElementById('computerChoiceLog'); 
+
+let declareWinnerText = document.getElementById('declareWinnerText'); 
+
+
+// Logic for a SINGLE ROUND (we will append the winner to the player / computer divs)
+
+function gameRound(playerChoice, computerChoice) { 
+    if (playerChoice == computerChoice) {
+        // we will append this to the 
+        declareWinnerText.innerHTML = `TIE GAME! YOU BOTH CHOSE ${computerChoice.toUpperCase()}`;
+    } else if (playerChoice == 'rock' && computerChoice == 'scissors') { 
+        // append ' 
+        declareWinnerText.innerHTML = 'YOU WIN! ROCK SMASHES SCISSORS';               
+    } else if (playerChoice == 'paper' && computerChoice == 'rock') {  
+        declareWinnerText.innerHTML = 'YOU WIN! PAPER COVERS ROCK';
+    } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        declareWinnerText.innerHTML = 'YOU WIN! SCISSORS CUTS PAPER';
+    } else if (computerChoice == 'rock' && playerChoice == 'scissors') { 
+        declareWinnerText.innerHTML = 'COMPUTER WINS! ROCK SMASHES SCISSORS';                     
+    } else if (computerChoice == 'paper' && playerChoice == 'rock') {  
+        declareWinnerText.innerHTML = 'COMPUTER WINS! PAPER COVERS ROCK';
+    } else if (computerChoice == 'scissors' && playerChoice == 'paper') {  
+        declareWinnerText.innerHTML = 'COMPUTER WINS! SCISSORS CUTS PAPER';  
+    }
+}
+
+rockBtn.addEventListener('click', function() {
+
+    playerChoice = 'rock';
+    computerChoice = getComputerChoice(); // logs rock, paper, or scissors 
+    
+    playerChoiceLog.innerHTML = playerChoice;
+    computerChoiceLog.innerHTML = computerChoice;
+
+    gameRound(playerChoice, computerChoice);
+    // ^ return 0
+    // add reset / refresh button
+    }
+);
+paperBtn.addEventListener('click', function() {
+
+    playerChoice = 'paper';
+    computerChoice = getComputerChoice(); // logs rock, paper, or scissors 
+    
+    playerChoiceLog.innerHTML = playerChoice;
+    computerChoiceLog.innerHTML = computerChoice;
+
+    gameRound(playerChoice, computerChoice);
+    // ^ return 0
+    // add reset / refresh button
+    }
+);
+scissorsBtn.addEventListener('click', function() {
+
+    playerChoice = 'scissors';
+    computerChoice = getComputerChoice(); // logs rock, paper, or scissors 
+    
+    playerChoiceLog.innerHTML = playerChoice;
+    computerChoiceLog.innerHTML = computerChoice;
+
+    gameRound(playerChoice, computerChoice);
+    // ^ return 0
+    // add reset / refresh button
+    }
+);
+
