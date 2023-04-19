0647d7813fb63cf5f76945f1c512c2b2fda8ba37
cfan310 <cfanning43@gmail.com>
2023-04-19 14:47:50 -0700

    added loop to display and append 16 boxes to letterContainer'

---
+++ sketch/app.js
@@ -0,0 +1,62 @@
+
+// container of the 16X16 grid  
+const gridContainer = document.getElementById('grid');  
+
+let divRow = document.createElement('div');
+
+let gridBox = document.createElement('div');  
+
+divRow.setAttribute('id', 'divRow');
+divRow.setAttribute('style', 'display: flex; flex-direction: row');
+
+
+/*
+gridBox.style.background = 'green';
+gridBox.style.height = '50px';
+gridBox.style.width = '50px';  
+*/
+
+//gridContainer.appendChild(divRow);
+//divRow.appendChild(gridBox); 
+  
+
+
+// loop through and display 16 in a single row
+// then create the 
+
+let gridArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+for (let i = 1; i <= gridArray.length - 1; i++) {
+
+    
+    let newSquare = document.createElement('div');
+    let squarePositionInRow = gridArray[i]; 
+    newSquare.style.background = 'green';
+    newSquare.style.height = '50px';
+    newSquare.style.width = '50px'; 
+    // append 16 to divRow
+    divRow.appendChild(newSquare); 
+
+}
+
+const letterContainer = document.getElementById('letterContainer');
+
+
+// we want to loop thru 10 numbers and append each one as a div (making
+// them siblings) to letterContainer
+
+for ( let i = 0; i < 16; i++) {
+    /*
+    let newLetter = document.createElement('div');
+    newLetter.innerHTML = 'A'; 
+    letterContainer.appendChild(newLetter);
+    */
+
+    let newBox = document.createElement('div'); 
+    newBox.innerHTML = 'div';
+    newBox.setAttribute('style','height: 50px; width: 50px; background-color: purple; border: solid 1px;');
+    letterContainer.appendChild(newBox);  
+
+
+
+}
